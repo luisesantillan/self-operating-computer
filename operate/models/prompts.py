c93d617e13b8b4eed2319c1ca3bef6a9217b7a37
Luis E. Santillan <144634192+luisesantillan@users.noreply.github.com>
2024-10-05 05:25:46 -0500

    Update prompts.py

--- operate/models/prompts.py
+++ operate/models/prompts.py
@@ -239,6 +239,13 @@ def get_system_prompt(model, objective):
             os_search_str=os_search_str,
             operating_system=operating_system,
         )
+    elif model == "gpt-4o-mini-with-ocr":
+        prompt = SYSTEM_PROMPT_OCR.format(
+            objective=objective,
+            cmd_string=cmd_string,
+            os_search_str=os_search_str,
+            operating_system=operating_system,
+        )
     elif model == "claude-3":
         prompt = SYSTEM_PROMPT_OCR.format(
             objective=objective,
